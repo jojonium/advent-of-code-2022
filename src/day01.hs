6726df29aeb0c7aedb2fa2a1204f155f4828a783
Joseph Petitti <joseph@petitti.org>
2022-11-30 21:56:16 -0800

    Clean up day 1

--- src/day01.hs
+++ src/day01.hs
@@ -10,13 +10,8 @@ getInput defFile = do
 main :: IO ()
 main = do
   input <- getInput "inputs/day01.txt"
-  let elves = splitOn "\n\n" input
-  putStrLn $ "Part 1: " ++ show (part1 elves)
-  putStrLn $ "Part 2: " ++ show (part2 elves)
-
-part1 :: [String] -> Integer
-part1 = maximum . map (sum . map read . lines)
-
-part2 :: [String] -> Integer
-part2 = sum . take 3 . sortBy (flip compare) . map (sum . map read . lines)
+  let paras = splitOn "\n\n" input
+      elves = (sortBy (flip compare) . map (sum . map read . lines)) paras
+  putStrLn $ "Part 1: " ++ show (head elves)
+  putStrLn $ "Part 2: " ++ show (sum (take 3 elves))
 

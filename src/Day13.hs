af33b56938b26b0c1995ddffdc2691de11ed3728
Joseph Petitti <joseph@petitti.org>
2022-12-13 16:28:09 -0500

    Clean up day 13

--- src/Day13.hs
+++ src/Day13.hs
@@ -2,7 +2,7 @@ module Day13 (main) where
 
 import System.Environment (getArgs)
 import Data.List.Split (splitOn)
-import Data.List (sortBy, elemIndex)
+import Data.List (sort, elemIndex)
 import Data.Maybe (fromJust)
 
 data PD = Leaf Int | Node [PD] deriving (Eq, Show)
@@ -19,13 +19,13 @@ main = do
   putStrLn $ "Part 2: " ++ show (part2 input)
 
 part1 :: String -> Int
-part1 = sum . map fst . filter ((==LT) . snd) . zip [1..] . map (uncurry compair . toPair) . splitOn "\n\n"
+part1 = sum . map fst . filter ((==LT) . snd) . zip [1..] . map (uncurry compare . toPair) . splitOn "\n\n"
 
 part2 :: String -> Int
 part2 input = index divA * index divB
   where divA    = parse "[[2]]"
         divB    = parse "[[6]]"
-        sortPs  = sortBy compair . (++ [divA, divB]) . map parse . filter (/= "") . lines
+        sortPs  = sort . (++ [divA, divB]) . map parse . filter (/= "") . lines
         packets = sortPs input
         index a = fromJust (elemIndex a packets) + 1
 
@@ -43,14 +43,14 @@ parseR xs       ps = let (token, rest) = break (`elem` ",][") xs
 toPair :: String -> (PD, PD)
 toPair s = let ls = lines s in (parse (head ls), parse (ls !! 1))
 
-compair :: PD -> PD -> Ordering
-compair (Leaf i) (Leaf j) = compare i j
-compair (Node (i:is)) (Node (j:js)) = case compair i j of
-  LT -> LT
-  GT -> GT
-  EQ -> compair (Node is) (Node js)
-compair (Node [])    (Node (_:_)) = LT
-compair (Node (_:_)) (Node [])    = GT
-compair (Node [])    (Node [])    = EQ
-compair (Leaf i)     (Node js)    = compair (Node [Leaf i]) (Node js)
-compair (Node is)    (Leaf j)     = compair (Node is) (Node [Leaf j])
+instance Ord PD where
+  compare (Leaf i) (Leaf j) = compare i j
+  compare (Node (i:is)) (Node (j:js)) = case compare i j of
+    LT -> LT
+    GT -> GT
+    EQ -> compare (Node is) (Node js)
+  compare (Node [])    (Node (_:_)) = LT
+  compare (Node (_:_)) (Node [])    = GT
+  compare (Node [])    (Node [])    = EQ
+  compare (Leaf i)     (Node js)    = compare (Node [Leaf i]) (Node js)
+  compare (Node is)    (Leaf j)     = compare (Node is) (Node [Leaf j])
